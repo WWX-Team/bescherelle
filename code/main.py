73b67293fa4d5d37621e372e8fa06fe4f2ad10ee
Anselme <106106763+wedego943@users.noreply.github.com>
2024-03-16 19:52:46 +0100

    Update main.py
    [add] début du prog tkinter pour la fenêtre

--- code/main.py
+++ code/main.py
@@ -20,5 +20,36 @@
 """
  - Initialisation du programme
 """
-###############################################################################
+import tkinter
 
+###############################################################################
+"""
+ - Création de la fenêtre
+"""
+window = tkinter.Tk() 
+
+"""
+ - Personnalisation
+"""
+window.title('Bescherelle')   # Nom à trouver
+window.geometry('1080x720')
+window.minsize(480, 360)
+#window.iconbitmap()
+window.config(background='#1c1c1c')
+
+
+"""
+ - Texte
+"""
+frame_title = tkinter.Frame(window, bg='#1c1c1c')
+
+label_title = tkinter.Label(frame_title, text='TITRE', font=('Arial', 60), bg='#1c1c1c', fg='#b21948')   # typo à trouver
+label_title.pack(expand='YES')
+
+frame_title.pack(side='top')
+
+
+"""
+ - Afficher 
+"""
+window.mainloop()
