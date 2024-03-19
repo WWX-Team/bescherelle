690342c0d20d3e9cf4612edd2856486cb9ce9835
Anselme <106106763+wedego943@users.noreply.github.com>
2024-03-19 15:52:57 +0100

    Update main.py
    [maj] update du tkinter

--- code/main.py
+++ code/main.py
@@ -23,25 +23,52 @@
 ###############################################################################
 # Import des MODULES
 import tkinter
+import scripts
 
+theme = {
+        'title'      : '#b21948',
+        'bg'         : '#000000',
+        'bg_input'   : '#b21948',
+        'text_input' : 'white',
+        'typo'       : 'Arial'
+        }
 ###############################################################################
 """
  - Gestion TKINTER
 """
+
 window = tkinter.Tk() 
+
 # Personnalisation
 window.title('Bescherelle')   # Nom à trouver
 window.geometry('1080x720')
 window.minsize(480, 360)
 #window.iconbitmap()
-window.config(background='#1c1c1c')
-# Texte
-frame_title = tkinter.Frame(window, bg='#1c1c1c')
-#
-label_title = tkinter.Label(frame_title, text='TITRE', font=('Arial', 60), bg='#1c1c1c', fg='#b21948')   # typo à trouver
-label_title.pack(expand='YES')
-#
-frame_title.pack(side='top')
+window.config(background=theme['bg'])
+              
+# Titre
+frame = tkinter.Frame(window, bg=theme['bg'])
+
+label_title = tkinter.Label(frame, text='TITRE', font=(theme['typo'], 60), bg=theme['bg'], fg=theme['title'])   # typo à trouver
+label_title.pack(pady=25, expand='YES')
+
+
+
+# Input 
+frame_input = tkinter.Frame(frame, bg=theme['bg'])
+                            
+verbe_input = tkinter.Entry(frame_input, font=(theme['typo'], 20), bg=theme['bg_input'], fg=theme['text_input'])
+verbe_input.pack()
+
+recherche_button = tkinter.Button(frame_input, text='Rechercher', font=(theme['typo'],20), bg=theme['bg_input'], fg=theme['text_input'], command=scripts.conjuguer)
+recherche_button.pack(pady=10, fill='x')
+
+frame_input.pack() 
+
+
+
+frame.pack(side='top')                           
+
 ###############################################################################
 """
  - Boucle principale
