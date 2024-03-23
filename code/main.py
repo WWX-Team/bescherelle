ac07b51dfc2bd95211a44f3a55d3d8b6f57015ec
Anselme <106106763+wedego943@users.noreply.github.com>
2024-03-23 18:14:13 +0100

    Update main.py
    [maj] couleurs

--- code/main.py
+++ code/main.py
@@ -26,37 +26,39 @@ import tkinter as tk
 from scripts import conjuguer
 
 theme = {
-        'title'      : '#b21948',
-        'bg'         : '#000000',
-        'bg_input'   : '#b21948',
-        'text_input' : 'white',
-        'typo'       : 'Helvetica'     # typo à trouver
+        'title'      : '#80002a',
+        'bg'         : '#1D1D1D',
+        'bg_entry'   : '#660022',
+        'bg_button'  : '#80002a',  
+        'text_entry' : 'white',
+        'text_button': 'white',
+        'typo'       : 'Helvetica'     
         }
 ###############################################################################
 """
  - Fonction affichage
 """
  
-def conjuguer_return ():
+def conjuguer_return():
     """
     affiche la fonction [conjuguer] de [scripts.py] après que l'utilisateur 
     ai cliqué sur [button_search]
     """
     verbe = entry_search.get()
     
-    frame_return = tk.Frame (
+    frame_return = tk.Frame(
                                      frame, 
                                      bg   = theme['bg']
-                                 )
+                            )
 
-    label_return = tk.Label ( 
+    label_return = tk.Label( 
                                      frame_return,
                                      text = conjuguer(verbe),
                                      justify = 'center',
                                      font = (theme['typo'], 20), 
                                      bg   = theme['bg'], 
                                      fg   = theme['title']
-                                 )
+                            )
     label_return.pack()
     
     frame_return.pack(side='bottom')
@@ -77,43 +79,46 @@ window.config(background=theme['bg'])
 # Titre
 frame = tk.Frame(window, bg=theme['bg'])
 
-label_title = tk.Label (
+label_title = tk.Label(
                                 frame, 
-                                text = 'TITRE', 
+                                text = 'Le Saint Bescherelle', 
                                 font = ('Impact', 60), 
                                 bg   = theme['bg'], 
                                 fg   = theme['title']
-                            )  
+                        )  
  
 label_title.pack(pady=25, expand='YES')
 
 
 
 # Input 
-frame_input = tk.Frame (
+frame_input = tk.Frame(
                                 frame, 
                                 bg = theme['bg']
-                            )
+                        )
                             
-entry_search = tk.Entry (
+entry_search = tk.Entry(
                                 frame_input, 
                                 font = (theme['typo'], 20), 
-                                bg   = theme['bg_input'], 
-                                fg   = theme['text_input']
-                             )
+                                bg   = theme['bg_entry'], 
+                                fg   = theme['text_entry']
+                        )
 entry_search.pack()
 
-button_search = tk.Button (
+button_search = tk.Button(
                                  frame_input, 
                                  text    = 'Rechercher', 
                                  font    = (theme['typo'],20), 
-                                 bg      = theme['bg_input'], 
-                                 fg      = theme['text_input'], 
+                                 bg      = theme['bg_button'], 
+                                 fg      = theme['text_button'], 
                                  command = conjuguer_return
-                               )
+                          )
 button_search.pack(pady=10, fill='x')
 
 frame_input.pack() 
+
+
+
 frame.pack(side='top')                           
 
 ###############################################################################
