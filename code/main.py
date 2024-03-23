e658afe1a6c40cd7ccb48b4ced5e30ef018ceac2
Anselme <106106763+wedego943@users.noreply.github.com>
2024-03-23 14:13:14 +0100

    Update main.py
    [maj] réarrangement pour que ça soit plus lisible

--- code/main.py
+++ code/main.py
@@ -30,7 +30,7 @@ theme = {
         'bg'         : '#000000',
         'bg_input'   : '#b21948',
         'text_input' : 'white',
-        'typo'       : 'Arial'
+        'typo'       : 'Arial'       # typo à trouver
         }
 ###############################################################################
 """
@@ -49,19 +49,42 @@ window.config(background=theme['bg'])
 # Titre
 frame = tkinter.Frame(window, bg=theme['bg'])
 
-label_title = tkinter.Label(frame, text='TITRE', font=(theme['typo'], 60), bg=theme['bg'], fg=theme['title'])   # typo à trouver
+label_title = tkinter.Label(
+                                frame, 
+                                text = 'TITRE', 
+                                font = (theme['typo'], 60), 
+                                bg   = theme['bg'], 
+                                fg   = theme['title']
+                            )  
+ 
 label_title.pack(pady=25, expand='YES')
 
 
 
 # Input 
-frame_input = tkinter.Frame(frame, bg=theme['bg'])
+frame_input = tkinter.Frame(
+                                frame, 
+                                bg   = theme['bg']
+                            )
                             
-entry_search = tkinter.Entry(frame_input, font=(theme['typo'], 20), bg=theme['bg_input'], fg=theme['text_input'])
+entry_search = tkinter.Entry(
+                                frame_input, 
+                                font = (theme['typo'], 20), 
+                                bg   = theme['bg_input'], 
+                                fg   = theme['text_input']
+                            )
 verbe = entry_search.get()
+
 entry_search.pack()
 
-button_search = tkinter.Button(frame_input, text='Rechercher', font=(theme['typo'],20), bg=theme['bg_input'], fg=theme['text_input'], command=scripts.conjuguer)
+button_search = tkinter.Button(
+                                 frame_input, 
+                                 text    = 'Rechercher', 
+                                 font    = (theme['typo'],20), 
+                                 bg      = theme['bg_input'], 
+                                 fg      = theme['text_input'], 
+                                 command = scripts.conjuguer(verbe)
+                              )
 button_search.pack(pady=10, fill='x')
 
 frame_input.pack() 
